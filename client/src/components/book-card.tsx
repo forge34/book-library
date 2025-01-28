15a7f767cb2daad5ec80e681eb6cd90973ecf4e9
Forge <28455546+forge34@users.noreply.github.com>
2025-01-28 16:35:31 +0200

    feat: add book card component

---
+++ client/src/components/book-card.tsx
@@ -0,0 +1,23 @@
+interface BookCardProps {
+  title: string;
+  imgUrl: string;
+  price: number;
+}
+
+export default function BookCard(props: BookCardProps) {
+  return (
+    <div className="bg-white flex flex-col rounded-4xl w-fit">
+      <div>
+        <img className="rounded-t-2xl" src={props.imgUrl} />
+      </div>
+
+      <div className="flex flex-col py-1 px-2">
+        <h3 className="text-xl text-primary font-bold">{props.title}</h3>
+        <p className="text-hover-accent font-bold">{props.price + " $"}</p>
+        <button className="bg-accent self-center rounded-2xl text-background py-1 px-3 font-black mb-2">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
