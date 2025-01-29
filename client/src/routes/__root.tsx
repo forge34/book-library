b5f8bb7e3c261908a73667afd905c02d1424e75b
forge34 <28455546+forge34@users.noreply.github.com>
2025-01-29 22:10:48 +0200

    feat: setup tanstack router

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,17 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Header from "../components/header";
+
+export const Route = createRootRoute({
+  component: Root,
+});
+
+function Root() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
