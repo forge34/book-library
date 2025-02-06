58752c76a1a210bd3e1c5b5e90ee1dde952ce41b
Forge <28455546+forge34@users.noreply.github.com>
2025-02-06 23:41:19 +0200

    feat: add first section ob $bookid page

--- client/src/components/book-card.tsx
+++ client/src/components/book-card.tsx
@@ -1,12 +1,25 @@
+import { useNavigate } from "@tanstack/react-router";
+
 interface BookCardProps {
   title: string;
   imgUrl: string;
   price: number;
+  bookid?: string;
 }
 
 export default function BookCard(props: BookCardProps) {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex w-fit flex-col rounded-4xl bg-white">
+    <div
+      className="flex w-fit flex-col rounded-4xl bg-white"
+      onClick={() => {
+        navigate({
+          to: `/books/$bookid`,
+          params: { bookid: props.bookid || "" },
+        });
+      }}
+    >
       <img
         className="w-full rounded-t-2xl"
         width={"205px"}
@@ -19,7 +32,7 @@ export default function BookCard(props: BookCardProps) {
         <p className="text-hover-accent text-md font-bold">
           {props.price + " $"}
         </p>
-        <button className="hover:brightness-115 hover:scale-110 transition-all duration-300 bg-accent text-background mt-auto mb-2 self-center rounded-2xl px-3 py-1 font-black">
+        <button className="bg-accent text-background mt-auto mb-2 self-center rounded-2xl px-3 py-1 font-black transition-all duration-300 hover:scale-110 hover:brightness-115">
           Add to cart
         </button>
       </div>
