2a86ded023c3900bd0ff0bfa2a1ce91d7323005d
forge34 <28455546+forge34@users.noreply.github.com>
2025-02-07 00:12:59 +0200

    feat: add related products section

--- client/src/routes/books.$bookid.tsx
+++ client/src/routes/books.$bookid.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import BookCard from "../components/book-card";
 
 export const Route = createFileRoute("/books/$bookid")({
   component: RouteComponent,
@@ -6,11 +7,11 @@ export const Route = createFileRoute("/books/$bookid")({
 
 function RouteComponent() {
   return (
-    <div className="flex flex-col px-4 py-2">
+    <div className="bg-background flex flex-col px-4 py-2">
       <div className="flex flex-col md:flex-row">
-        <img src="https://placehold.co/400x500" className="rounded-2xl" />
+        <img src="https://placehold.co/300x400" className="rounded-2xl" />
 
-        <div className="mx-4 flex flex-col md:px-4 py-2">
+        <div className="mx-4 flex flex-col py-2 md:mx-8 md:px-4">
           <h2 className="text-primary text-2xl font-bold tracking-wider">
             Atomic Habits
           </h2>
@@ -26,7 +27,44 @@ function RouteComponent() {
             <button className="bg-primary mx-2 rounded-lg px-8 py-2 text-white">
               Buy now
             </button>
+            <hr className="bg-primary" />
           </div>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <h1 className="text-primary text-2xl font-bold">Related products</h1>
+        <div className="mt-4 flex flex-col justify-between gap-4 md:flex-row">
+          <BookCard
+            title="Atomic habits"
+            price={20}
+            bookid="1"
+            imgUrl="https://placehold.co/200x250"
+          />
+          <BookCard
+            title="how to make friends and influence people "
+            price={20}
+            bookid="2"
+            imgUrl="https://placehold.co/200x250"
+          />
+          <BookCard
+            title="Atomic habits"
+            price={20}
+            bookid="3"
+            imgUrl="https://placehold.co/200x250"
+          />
+          <BookCard
+            title="Atomic habits"
+            price={20}
+            bookid="4"
+            imgUrl="https://placehold.co/200x250"
+          />
+          <BookCard
+            title="Atomic habits"
+            price={20}
+            bookid="5"
+            imgUrl="https://placehold.co/200x250"
+          />
         </div>
       </div>
     </div>
