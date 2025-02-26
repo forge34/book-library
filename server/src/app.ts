856022a8700767c36b9f65383c8072283f98c4bf
forge34 <28455546+forge34@users.noreply.github.com>
2025-02-26 21:22:11 +0200

    feat(server): initial setup

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors, { CorsOptions } from "cors";
+import express, { Express } from "express";
+import morgan from "morgan";
+
+const app: Express = express();
+
+export const corsOptions: CorsOptions = {
+  origin: ["http://localhost:5173", process.env.CLIENT_URL],
+  credentials: true,
+  allowedHeaders: ["Content-type"],
+};
+
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export { app };
