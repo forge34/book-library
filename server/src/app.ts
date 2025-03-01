bf4d68caebd6c865f14ea5aeb24240b65a012bbc
Forge <28455546+forge34@users.noreply.github.com>
2025-03-02 00:36:59 +0200

    feat(server): initial router setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors, { CorsOptions } from "cors";
 import express, { Express } from "express";
 import morgan from "morgan";
+import router from "./routes/index";
 
 const app: Express = express();
 
@@ -18,8 +19,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(router);
 
 export { app };
