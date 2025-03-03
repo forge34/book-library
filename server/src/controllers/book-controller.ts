5088104ae5d89e17e3953203a02a9ab5711c43b0
forge34 <28455546+forge34@users.noreply.github.com>
2025-03-03 17:13:32 +0200

    feat(server): add seed script

--- server/src/controllers/book-controller.ts
+++ server/src/controllers/book-controller.ts
@@ -1,6 +1,6 @@
 import expressAsyncHandler from "express-async-handler";
 import { Request, Response } from "express";
-import { prisma } from "../config/prisma";
+import prisma from "../config/prisma";
 
 const getAllBooks = expressAsyncHandler(async (req: Request, res: Response) => {
   const books = await prisma.book.findMany({});
