575dcaf38258255d7b6debbc54638b9bc30db45f
Forge <28455546+forge34@users.noreply.github.com>
2025-03-07 02:04:51 +0200

    feat(client): link single book page with api

--- packages/client/src/routes/books.$bookid.tsx
+++ packages/client/src/routes/books.$bookid.tsx
@@ -1,22 +1,37 @@
 import { createFileRoute } from "@tanstack/react-router";
 import BookCard from "../components/book-card";
+import type { bookWithAuthor } from "shared";
+
+const fetchBook = async (bookid: string): Promise<bookWithAuthor> => {
+  const res = await fetch(`${import.meta.env.VITE_API}/books/${bookid}`);
+
+  return res.json();
+};
 
 export const Route = createFileRoute("/books/$bookid")({
+  loader: ({ params: { bookid } }) => fetchBook(bookid),
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const book = Route.useLoaderData();
   return (
     <div className="bg-background flex flex-col px-4 py-2">
       <div className="flex flex-col md:flex-row">
-        <img src="https://placehold.co/300x400" className="rounded-2xl" />
+        <img
+          src={book.coverImage?.replace("M", "L") || ""}
+          className="mx-auto h-[442px] w-[332px] rounded-2xl md:mx-0"
+        />
 
-        <div className="mx-4 flex flex-col py-2 md:mx-8 md:px-4">
-          <h2 className="text-primary text-2xl font-bold tracking-wider">
-            Atomic Habits
+        <div className="mx-7 flex flex-col py-2 md:mx-8 md:px-4">
+          <h2 className="text-primary text-2xl md:text-4xl font-bold tracking-wider">
+            {book.name}
           </h2>
 
-          <p className="text-accent my-2 text-2xl font-bold">20 $</p>
+          <p className="font-light mt-2">{book.description}</p>
+          <p className="text-accent my-2 text-xl md:text-2xl font-bold">
+            {book.price + " $"}
+          </p>
 
           <div className="flex flex-row flex-wrap md:flex-nowrap">
             <input type="number" />
@@ -28,6 +43,22 @@ function RouteComponent() {
               Buy now
             </button>
             <hr className="bg-primary" />
+          </div>
+          <div className="flex flex-col justify-around mt-5">
+            <h3 className="mt-4">
+              Author : <span className="font-light">{book.author?.name}</span>
+            </h3>
+            <h3 className="mt-4">
+              Page Count :{" "}
+              <span className="font-light">{book.pages} Pages</span>
+            </h3>
+
+            <h3 className="mt-4">
+              Paper : <span className="font-light">70 GSM</span>
+            </h3>
+            <h3 className="mt-4">
+              Cover : <span className="font-light">140 GSM (Hard Cover)</span>
+            </h3>
           </div>
         </div>
       </div>
